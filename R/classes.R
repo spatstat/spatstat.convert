c40f6796cb9ce7efbe0c4e9307e50319b1b4dc7e
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2026-08-20 23:13:38 +0800

    Fleshed out package information

--- R/classes.R
+++ R/classes.R
@@ -6,3 +6,15 @@ if (!isClass("im")) setClass("im")
 if (!isClass("tess")) setClass("tess")
 if (!isClass("linnet")) setClass("linnet")
 
+## if (!isClass("SpatRaster")) setClass("SpatRaster")
+
+## if (!isClass("sf")) setClass("sf")
+
+## if (!isClass("Line")) setClass("Line")
+## if (!isClass("Lines")) setClass("Lines")
+## if (!isClass("SpatialLines")) setClass("SpatialLines")
+## if (!isClass("SpatialLinesDataFrame")) setClass("SpatialLinesDataFrame")
+## if (!isClass("SpatialGridDataFrame")) setClass("SpatialGridDataFrame")
+## if (!isClass("SpatialPolygons")) setClass("SpatialPolygons")
+## if (!isClass("SpatialPoints")) setClass("SpatialPoints")
+## if (!isClass("SpatialPointsDataFrame")) setClass("SpatialPointsDataFrame")
